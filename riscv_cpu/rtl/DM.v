beb42e1dafaa68d029cfd303e266e35e668f23f8
Yuechuovo <159030982+Yuechu-23@users.noreply.github.com>
2026-04-18 13:53:31 +0800

    同步写，同步读 → 同步写，异步读

--- riscv_cpu/rtl/DM.v
+++ riscv_cpu/rtl/DM.v
@@ -1,19 +1,23 @@
 `include "ctrl_signal_def.v"
 
-module DM(Addr, WD, clk, DMCtrl, RD);
-    input [11:2] Addr;
-    input [31:0] WD;
-    input clk;
-    input DMCtrl;
-    output reg [31:0] RD;
+module DM(
+    input  [11:2] Addr,
+    input  [31:0] WD,
+    input  clk,
+    input  DMCtrl,
+    output [31:0] RD
+);
 
     reg [31:0] memory[0:1023];
+
+    // 同步写
     always @(posedge clk) begin
-        if (DMCtrl) begin
+        if (DMCtrl == `DMCtrl_WR) begin
             memory[Addr] <= WD;
         end
-        else begin
-            RD <= memory[Addr];
-        end
     end
+
+    // 异步读
+    assign RD = (DMCtrl == `DMCtrl_RD) ? memory[Addr] : 32'b0;
+
 endmodule
